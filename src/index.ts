d76c32d9cb3adc938b5a55b8bc18b58f8fdff1bf
Albert Gergo <gergoalbert.gta@gmail.com>
2020-06-18 16:19:47 +0300

    Basic routing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import express from "express"
+import routes from "./server/routes/routes"
 
 const app = express();
 const PORT = 3000;
 
+app.set("view engine", "ejs")
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
+
+routes(app)
---
+++ src/server/routes/routes.ts
@@ -0,0 +1,10 @@
+import * as express from "express"
+
+export default function routes(app: express.Application) {
+    app.get("/", (req, res) => {
+        res.render("index")
+    })
+    app.get("/room/:id", (req, res) => {
+        res.render("room")
+    })
+}
