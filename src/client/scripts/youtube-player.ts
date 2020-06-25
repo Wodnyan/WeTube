183c22e3d6b03e1ab2288234d34f3b0ecfacc489
Albert Gergo <gergoalbert.gta@gmail.com>
2020-06-25 14:01:01 +0300

    Changed the youtube-player script to typescript

--- src/client/scripts/youtube-player.ts
+++ src/client/scripts/youtube-player.ts
@@ -1,8 +1,8 @@
 const socket = io();
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement("script");
-const windowWidth = window.innerWidth * 0.7;
-const windowHeight = window.innerHeight * 0.7;
+const windowWidth = window.innerWidth * 0.75;
+const windowHeight = window.innerHeight * 0.75;
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
@@ -10,7 +10,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
+var player: YT.Player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player("player", {
         height: windowHeight,
@@ -24,26 +24,18 @@ function onYouTubeIframeAPIReady() {
 }
 
 // 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
+function onPlayerReady(event: YT.PlayerEvent) {
     event.target.playVideo();
+    console.log("Ready");
 }
 
-function onPlayerStateChange(event) {
-    if (event.data === 1) {
-        socket.emit("started video", window.location.pathname);
-    } else if (event.data === 2) {
-        socket.emit("paused video", window.location.pathname);
+function onPlayerStateChange(event: YT.PlayerEvent) {
+    switch (event.data) {
+        case 1:
+            socket.emit("started video", window.location.pathname, event.target.getCurrentTime);
+            break;
+        case 2:
+            socket.emit("paused video", window.location.pathname);
+            break;
     }
-}
-
-//Made functions so that
-//TS doesn't yell at me
-function loadVideo(id) {
-    player.loadVideoById(id);
-}
-function startVideo() {
-    player.playVideo();
-}
-function pauseVideo() {
-    player.pauseVideo();
 }
